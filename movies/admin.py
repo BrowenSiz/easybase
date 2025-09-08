6debf331042b9f79cd8e6b4271f387942dc987ae
BrowenSiz <top.timashevsky@gmail.com>
2025-09-08 19:23:57 +0300

    feat: Настроили админ панель с помощью наборов атрибутов

--- movies/admin.py
+++ movies/admin.py
@@ -5,7 +5,25 @@ from .models import Genre, Movie, Director
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'release_year', 'rating', 'is_published', 'director')
     list_filter = ('is_published', 'genres', 'release_year')
-    search_fields = ('title', 'description')
+    search_fields = ('title',)
+
+    readonly_fields = ('time_create', 'time_update')
+
+    fieldsets = (
+        ('Основная ифнормация', {
+            'fields': ('title', 'description', 'poster')
+        }),
+        ('Данные о релизе', {
+            'fields': (('release_year', 'rating'),)
+        }),
+        ('Связи', {
+            'fields': ('director', 'genres')
+        }),
+        ('Статус', {
+            'classes': ('collapse',),
+            'fields': ('is_published', 'time_create', 'time_update')
+        }),
+    )
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
